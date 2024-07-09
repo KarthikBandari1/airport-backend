009c7a338c21c96a4db8b5bf9190c130c6b780f1
karthik <karthik@gmail.com>
2024-07-09 22:08:40 +0530

    deploy commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const cors = require('cors');
+
 
 const airportRoutes = require('./routes/airportRoutes');
-
+app.use(cors());
 app.use('/api', airportRoutes);
 
 app.listen(port, () => {
